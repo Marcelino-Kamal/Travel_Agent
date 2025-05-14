0679ac1cf150f097910ef9fd9b7c01c11bacb705
Marcelino <marconaoum@gmail.com>
2025-05-14 12:32:32 +0300

    Add project files.

---
+++ TravelAgent/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelAgent.Models;
+
+namespace TravelAgent.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options){ }
+
+        public DbSet<TravelPackages> TravelPackages { get; set; }
+
+        
+    }
+}
---
+++ TravelAgent/Models/TravelPackages.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace TravelAgent.Models
+{
+    public class TravelPackages
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string  ImgUrl { get; set; }
+
+        [Required]
+        [Precision(18,2)]
+        public decimal price { get; set; }
+            
+        [Required]
+        public DateTime DateTime { get; set; }
+
+
+
+
+    }
+}
