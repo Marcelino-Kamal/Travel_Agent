aef0b1c87d00a0760fb45d92c8353e32d2c9f416
Marcelino <marconaoum@gmail.com>
2025-05-14 14:20:04 +0300

    Controller

---
+++ TravelAgent/Controllers/CatalogController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TravelAgent.Data;
+
+namespace TravelAgent.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CatalogController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CatalogController(ApplicationDbContext context) { 
+        
+            _context = context;
+        
+        }
+
+        [HttpGet("TravelPackages")]
+        public async Task<IActionResult> GetAll()
+        {
+            var packages = await _context.TravelPackages.ToListAsync();
+            return Ok(packages);
+            
+        }
+
+    }
+}
