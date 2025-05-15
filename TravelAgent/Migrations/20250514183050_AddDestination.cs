1dc3b590efec81bc720ff4b919362d4ce98c8d2a
Marcelino <marconaoum@gmail.com>
2025-05-15 21:03:55 +0300

    Migration

---
+++ TravelAgent/Migrations/20250514183050_AddDestination.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelAgent.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDestination : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Destination",
+                table: "TravelPackages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Destination",
+                table: "TravelPackages");
+        }
+    }
+}
